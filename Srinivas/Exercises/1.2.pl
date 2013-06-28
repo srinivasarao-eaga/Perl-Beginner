60063733f9cd1ae2d0641bcd438f6f17327d8e46
srinivasarao-eaga <srinivasarao.eaga@gmail.com>
2013-06-28 12:54:14 +0530

    Update 1.2.pl

--- Srinivas/Exercises/1.2.pl
+++ Srinivas/Exercises/1.2.pl
@@ -7,8 +7,8 @@
 # Not defined, so zero by default
 my $radius = undef;  
  
-# STDIN is the line input operator. Reads online at a time from the console
-# chomp trims the newline at  the end
+# STDIN is the line input operator. Reads one line at a time from the console
+# chomp trims the newline at  the end, only one new line though
 chomp($radius = <STDIN>); 
  
 my $circumference = 2 * 3.14 * $radius;
