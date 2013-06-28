16159556d782b4e5842e7b21aeba91c78e8833d6
srinivasarao-eaga <srinivasarao.eaga@gmail.com>
2013-06-28 13:34:10 +0530

    Update 1.1.pl
    Added comments

--- Srinivas/Exercises/1.1.pl
+++ Srinivas/Exercises/1.1.pl
@@ -1,7 +1,16 @@
 #!/usr/bin/perl
 
 # 1.1 - Circumference of the circle
- 
+
+# Notice the first line of the program. Its called 'shebang' or 'hashbang'
+# When 'shebang' is the first two bytes of the program, the program loader
+# treats the rest of the line as 'Interpreter directive', meaning which 
+# interpreter to use to run the rest of the program.
+
+
+# The three datatypes available in Perl are scalars denoted by '$' sign, arrays 
+# denoted by '@' sign and hashes denoted by '%' sign.
+# Declaration of a variable needs 'my' keyword.
 my $radius = 12.5;
  
 my $circumference = 2 * 3.14 * $radius;
