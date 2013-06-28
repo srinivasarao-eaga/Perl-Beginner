87d1e24e6901421c24cba2229ffa9c70ea81b2e3
srinivasarao-eaga <srinivasarao.eaga@gmail.com>
2013-06-28 12:40:17 +0530

    Create 1.1.pl

---
+++ Srinivas/Exercises/1.1.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+# 1.1 - Circumference of the circle
+ 
+my $radius = 12.5;
+ 
+my $circumference = 2 * 3.14 * $radius;
+ 
+print "The circumference of a circle with $radius units of radius is $circumference";
