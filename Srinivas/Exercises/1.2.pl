886ebc95908656ea964ecac0889f30faa4d227b3
srinivasarao-eaga <srinivasarao.eaga@gmail.com>
2013-06-28 12:53:28 +0530

    Update 1.2.pl
    Added comments

--- Srinivas/Exercises/1.2.pl
+++ Srinivas/Exercises/1.2.pl
@@ -1,11 +1,17 @@
 #!/usr/bin/perl
 
- # 1.2 - Circumference of the circle, asking for user input
+# 1.2 - Circumference of the circle, asking for user input
+
+# Unintialised variables in Perl have 'undef' value by default
+# 'undef' will be zero in integer context, empty string in string context
+# Not defined, so zero by default
+my $radius = undef;  
  
- my $radius = undef;  # Not defined, so zero by default
+# STDIN is the line input operator. Reads online at a time from the console
+# chomp trims the newline at  the end
+chomp($radius = <STDIN>); 
  
- chomp($radius = <STDIN>);
+my $circumference = 2 * 3.14 * $radius;
  
- my $circumference = 2 * 3.14 * $radius;
- 
- print "The circumference of a circle with $radius units of radius is $circumference";
+# 'String Interpolation' happens is double quoted strings but not in single quoted ones 
+print "The circumference of a circle with $radius units of radius is $circumference";
