f933a95c7ed4e52550de3c700a0ac527cf6fadbc
srinivasarao-eaga <srinivasarao.eaga@gmail.com>
2013-06-28 15:43:33 +0530

    Update 1.5.pl

--- Srinivas/Exercises/1.5.pl
+++ Srinivas/Exercises/1.5.pl
@@ -8,10 +8,17 @@ my $repeatCount;
 chomp($inputString = <STDIN>);
 chomp($repeatCount = <STDIN>);
 
+if($repeatCount < 0 )
+{
+ print "Can't repeat the string $repeatCount times. Not atleast in Perl \n";
+ exit;
+ print "I'm dead Jim. You can't see me. Are we zombies? \n";
+}
+
 # Append a new line at the end of input string
 # Most of the binary operators can use the shorthand notation with assignment
 
 $inputString .= "\n";
 
 # 'x' is a string repeatition operator
-print $inputString x $repeatCount;
+print $inputString x $repeatCount . "\n";
