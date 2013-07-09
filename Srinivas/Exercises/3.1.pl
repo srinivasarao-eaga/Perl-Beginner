e78604d8f29e03164dcdec4163c485b420d0dd71
srinivasarao-eaga <srinivasarao.eaga@gmail.com>
2013-07-09 17:25:59 +0530

    Create 3.1.pl

---
+++ Srinivas/Exercises/3.1.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+my @numbers = <STDIN>;
+chomp(@numbers);
+
+// &sum(@numbers) is the function call
+print "The sum of @numbers is - " . &sum(@numbers);
+
+# This is a sub routine. The arguments are passed in an 
+# array called @_ underneath
+
+sub sum
+{
+  my $sum = 0;
+  
+  foreach (@_)
+  {
+    $sum += $_ ;   # $_ is the default variable used if not specified
+  }
+  $sum;            # Perl returns the last expresssion by default
+}
